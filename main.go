e7e02f53a28edaa6316e0a521f22ed9c94d27ab3
rmbarboza <rodrigo@rodrigopredator.localdomain>
2026-08-10 11:29:05 -0300

    Add concurrency with goroutines and channels

--- main.go
+++ main.go
@@ -25,46 +25,6 @@ type weatherUnderground struct {
 
 type multiWeatherProvider []weatherProvider
 
-func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Println("No .env file found, reading from system env")
-	}
-
-	// Read the key
-	openWeatherMapApiKey := os.Getenv("OPEN_WEATHER_MAP_APPID")
-	if openWeatherMapApiKey == "" {
-		log.Fatal("OPEN_WEATHER_MAP_APPID is not set")
-	}
-
-	mw := multiWeatherProvider{
-		openWeatherMap{apiKey: openWeatherMapApiKey},
-		weatherUnderground{apiKey: "your-key-here"},
-	}
-
-	http.HandleFunc("/hello", hello)
-
-	http.HandleFunc("/weather/", func(w http.ResponseWriter, r *http.Request) {
-		begin := time.Now()
-		city := strings.SplitN(r.URL.Path, "/", 3)[2]
-
-		temp, err := mw.temperature(city)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		w.Header().Set("Content-Type", "application/json; charset=utf-8")
-		json.NewEncoder(w).Encode(map[string]interface{}{
-			"city": city,
-			"temp": temp,
-			"took": time.Since(begin).String(),
-		})
-	})
-
-	http.ListenAndServe(":8080", nil)
-}
-
 func hello(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("hello!"))
 }
@@ -117,23 +77,76 @@ func (w weatherUnderground) temperature(city string) (float64, error) {
 }
 
 func (w multiWeatherProvider) temperature(city string) (float64, error) {
-	sum := 0.0
+	// Make a channel for temperatures, and a channel for errors.
+	// Each provider will push a value into only one.
+	temps := make(chan float64, len(w))
+	errs := make(chan error, len(w))
 
+	// For each provider, spawn a goroutine with an anonymous function.
+	// That function will invoke the temperature method, and forward the response.
 	for _, provider := range w {
-		k, err := provider.temperature(city)
-		if err != nil {
-			return 0, err
-		}
-
-		sum += k
+		go func(p weatherProvider) {
+			k, err := p.temperature(city)
+			if err != nil {
+				errs <- err
+				return
+			}
+			temps <- k
+		}(provider)
 	}
 
+	sum := 0.0
+
+	// Collect a temperature or an error from each provider.
+	for i := 0; i < len(w); i++ {
+		select {
+		case temp := <-temps:
+			sum += temp
+		case err := <-errs:
+			return 0, err
+		}
+	}
+
+	// Return the average, same as before.
 	return sum / float64(len(w)), nil
 }
 
-type weatherData struct {
-	Name string `json:"name"`
-	Main struct {
-		Kelvin float64 `json:"temp"`
-	} `json:"main"`
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No .env file found, reading from system env")
+	}
+
+	// Read the key
+	openWeatherMapApiKey := os.Getenv("OPEN_WEATHER_MAP_APPID")
+	if openWeatherMapApiKey == "" {
+		log.Fatal("OPEN_WEATHER_MAP_APPID is not set")
+	}
+
+	mw := multiWeatherProvider{
+		openWeatherMap{apiKey: openWeatherMapApiKey},
+		weatherUnderground{apiKey: "your-key-here"},
+	}
+
+	http.HandleFunc("/hello", hello)
+
+	http.HandleFunc("/weather/", func(w http.ResponseWriter, r *http.Request) {
+		begin := time.Now()
+		city := strings.SplitN(r.URL.Path, "/", 3)[2]
+
+		temp, err := mw.temperature(city)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		json.NewEncoder(w).Encode(map[string]interface{}{
+			"city": city,
+			"temp": temp,
+			"took": time.Since(begin).String(),
+		})
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
