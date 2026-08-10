705ee2b87cddd7751cb6df668506e2911095d294
rmbarboza <rodrigo@rodrigopredator.localdomain>
2026-08-10 10:02:30 -0300

    Get weather data from open weather map

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/rmbarboza/gofirstproject
+
+go 1.26.5
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- main.go
+++ main.go
@@ -1,12 +1,68 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	http.HandleFunc("/", hello)
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No .env file found, reading from system env")
+	}
+
+	// Read the key
+	apiKey := os.Getenv("OPEN_WEATHER_MAP_APPID")
+	if apiKey == "" {
+		log.Fatal("OPEN_WEATHER_MAP_APPID is not set")
+	}
+	http.HandleFunc("/hello", hello)
+
+	http.HandleFunc("/weather/", func(w http.ResponseWriter, r *http.Request) {
+		city := strings.SplitN(r.URL.Path, "/", 3)[2]
+
+		data, err := query(city, apiKey)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		json.NewEncoder(w).Encode(data)
+	})
+
 	http.ListenAndServe(":8080", nil)
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("hello!"))
+}
+
+func query(city string, apiKey string) (weatherData, error) {
+	resp, err := http.Get("http://api.openweathermap.org/data/2.5/weather?APPID=" + apiKey + "&q=" + city)
+	if err != nil {
+		return weatherData{}, err
+	}
+
+	defer resp.Body.Close()
+
+	var d weatherData
+
+	if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
+		return weatherData{}, err
+	}
+
+	return d, nil
+}
+
+type weatherData struct {
+	Name string `json:"name"`
+	Main struct {
+		Kelvin float64 `json:"temp"`
+	} `json:"main"`
 }
